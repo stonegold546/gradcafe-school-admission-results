96e9c1001f0361f3f9befd76ddf446e57c405927
James Uanhoro <stonegold546@gmail.com>
2016-02-05 16:05:03 +0800

    ewfd

--- workers/worker.rb
+++ workers/worker.rb
@@ -18,7 +18,7 @@ class GradCafeWorker
     number_of_pages = calculate_number_of_pages(page_one)
     if number_of_pages == 0
       publish([0, 0].to_json)
-      return # 'No results for your query'
+      return 'No results for your query'
     end
     other_pages = go_through_other_pages(number_of_pages) if number_of_pages > 1
     [page_one] + other_pages
