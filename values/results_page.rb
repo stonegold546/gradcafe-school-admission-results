ae18fa5d4d53693b5ffe0ce5c2c8040e02369a1b
James Uanhoro <stonegold546@gmail.com>
2016-02-13 11:30:56 +0800

    Cleaned up results.slim file
    - Created 'value objects' for the slim page.

---
+++ values/results_page.rb
@@ -0,0 +1,68 @@
+require 'descriptive_statistics'
+
+TIMER = {
+  'n' => 'today only', 't' => 'past two days', 'w' => 'past week',
+  'm' => 'past month', 'a' => 'all time'
+}
+
+# Object to create all the necessary results for the results page
+class ResultsPage
+  def initialize(accept_reject, time_period)
+    @accepted = accept_reject[0]
+    @rejected = accept_reject[1]
+    @time_period = TIMER[time_period]
+  end
+
+  def call
+    accepted_column = column_data(@accepted)
+    accepted_column = column_data_default(accepted_column)
+    rejected_column = column_data(@rejected)
+    rejected_column = column_data_default(rejected_column)
+    accepted_box = box_data(@accepted)
+    rejected_box = box_data(@rejected)
+    [
+      accepted_column, rejected_column,
+      accepted_box, rejected_box, @time_period
+    ]
+  end
+
+  def generate_boxplot_array(arr)
+    result = [arr.descriptive_statistics[:min], arr.percentile(25), arr.median,
+              arr.percentile(75), arr.descriptive_statistics[:max]]
+    result = [0, 0, 0, 0, 0] if result.any? { |e| e.to_s == 'NaN' || e.nil? }
+    result
+  end
+
+  def column_data(global_item, global_item_gre = {}, global_item_gpa_awa = {})
+    global_item.each_key do |key|
+      if [:gre_quant, :gre_verbal].include? key
+        global_item_gre[key] = global_item[key].mean
+      elsif [:gre_awa, :undergraduate_gpa].include? key
+        global_item_gpa_awa[key] = global_item[key].mean
+      end
+    end
+    [global_item_gre, global_item_gpa_awa]
+  end
+
+  def column_data_default(gre_gpa_awa)
+    gre_gpa_awa.each do |item|
+      item.each_key do |key|
+        item[key] ||= 0
+        item[key] = 0 if item[key].to_s == 'NaN'
+      end
+    end
+    gre_gpa_awa
+  end
+
+  def box_data(global_item, global_item_gre_box = {},
+               global_item_gpa_awa_box = {})
+    global_item.each_key do |key|
+      if [:gre_quant, :gre_verbal].include? key
+        global_item_gre_box[key] = generate_boxplot_array(global_item[key])
+      elsif [:gre_awa, :undergraduate_gpa].include? key
+        global_item_gpa_awa_box[key] = generate_boxplot_array(global_item[key])
+      end
+    end
+    [global_item_gre_box, global_item_gpa_awa_box]
+  end
+end
---
+++ values/results_page_accept_reject_nation.rb
@@ -0,0 +1,88 @@
+# Object to display slim results
+class ResultsPageAcceptRejectNation
+  def initialize(a_r_nation)
+    @a_r_nation = a_r_nation
+    @a_r_nation_visual = Hash.new { |hash, key| hash[key] = {} }
+    @q_array = [[], []]
+    @v_array = [[], []]
+    @gpa_array = [[], []]
+    @awa_array = [[], []]
+  end
+
+  def call
+    go_through_a_r_mean_box
+    go_through_a_r_delete_nulls
+    go_through_case
+    delete_gre_subject
+    [@q_array, @v_array, @gpa_array, @awa_array, @a_r_nation_visual]
+  end
+
+  def go_through_a_r_mean_box
+    @a_r_nation.each_key do |key|
+      @a_r_nation[key].each_key do |stat|
+        @a_r_nation_visual[key]["#{stat}_mean"] = @a_r_nation[key][stat].mean
+        @a_r_nation_visual[key]["#{stat}_box"] =
+        generate_boxplot_array(@a_r_nation[key][stat])
+      end
+    end
+  end
+
+  def go_through_a_r_delete_nulls
+    @a_r_nation.each_key do |key|
+      @a_r_nation[key].each_key do |stat|
+        @a_r_nation_visual[key]["#{stat}_mean"] ||= 0
+        @a_r_nation_visual[key]["#{stat}_mean"] = 0 if
+          ['', 'NaN'].include? @a_r_nation_visual[key]["#{stat}_mean"].to_s
+      end
+    end
+  end
+
+  def generate_boxplot_array(arr)
+    result = [arr.descriptive_statistics[:min], arr.percentile(25), arr.median,
+              arr.percentile(75), arr.descriptive_statistics[:max]]
+    result = [0, 0, 0, 0, 0] if result.any? { |e| e.to_s == 'NaN' || e.nil? }
+    result
+  end
+
+  def go_through_case
+    @a_r_nation.each_key do |key|
+      @a_r_nation[key].each_key do |stat|
+        if key.to_s.include? 'accept'
+          accept_reject_case_quant_verbal(key, stat, 0)
+          accept_reject_case_awa_gpa(key, stat, 0)
+        elsif key.to_s.include? 'reject'
+          accept_reject_case_quant_verbal(key, stat, 1)
+          accept_reject_case_awa_gpa(key, stat, 1)
+        end; end; end
+  end
+
+  def accept_reject_case_quant_verbal(key, stat, zero_one)
+    case stat.to_s
+    when 'gre_quant' then @q_array[zero_one] <<
+      [last_letter_upcase(key), @a_r_nation_visual[key]["#{stat}_mean"]]
+    when 'gre_verbal' then @v_array[zero_one] <<
+      [last_letter_upcase(key), @a_r_nation_visual[key]["#{stat}_mean"]]
+    end
+  end
+
+  def accept_reject_case_awa_gpa(key, stat, zero_one)
+    case stat.to_s
+    when 'undergraduate_gpa' then @gpa_array[zero_one] <<
+      [last_letter_upcase(key), @a_r_nation_visual[key]["#{stat}_mean"]]
+    when 'gre_awa' then @awa_array[zero_one] <<
+      [last_letter_upcase(key), @a_r_nation_visual[key]["#{stat}_mean"]]
+    end
+  end
+
+  def last_letter_upcase(key)
+    key.to_s[-1].upcase
+  end
+
+  def delete_gre_subject
+    @a_r_nation_visual.each_key do |key|
+      @a_r_nation_visual[key].each_key do |stat|
+        @a_r_nation_visual[key].delete(stat) if
+          %w(gre_subject_mean gre_subject_box).include? stat
+      end; end
+  end
+end
